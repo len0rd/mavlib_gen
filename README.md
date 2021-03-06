90ab35cb98c22c947ee1b7ad853a87e14b11bc73
len0rd <len0rd@users.noreply.github.com>
2021-03-06 00:29:34 -0700

    add a bunch of test cases for the schema validator

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-Well-tested, more feature complete MAVLink C generator. Built to be extensible to other languages!
+A mavlink C lang generator with a permissive MIT license
+
+## Features
+
+- [Capable of generation based on complex include trees](#complex-include-trees)
+- Simple, lean helper methods
+- Thorough unit tests
+
+### Complex Include Trees
+
+One feature I've wanted in pymavlink for quite some time is the ability to generate
+mavlink code from more advanced include trees, such as the one below.
+
+**ADVANCED TREE HERE**
+
+Such support allows developers to better separate/isolate specific modules of a
+messaging system. Thus avoiding large, hard to navigate files.
+
+### Simple, lean helper methods
+
+*Stuff about parsing based on char arrays instead of single character*
--- tests/xml_validator_tests/validator_tests.py
+++ tests/xml_validator_tests/validator_tests.py
@@ -4,18 +4,32 @@ import xmlschema
 class TestXmlSchemaValidator(unittest.TestCase):
 
     def test_failure_cases(self):
-        """Test cases where we expect the schema validator to produce and error"""
+        """Test cases where the schema validator should produce an error"""
         script_dir = os.path.dirname(__file__)
         base_url = os.path.abspath(os.path.join(script_dir, '..', '..', 'leo_mavgen', 'schema'))
         schema = xmlschema.XMLSchema11(os.path.join(base_url, 'mavlink_schema.xsd'), base_url=base_url)
 
-        # schema.validate(os.path.join(script_dir, 'test_cases', 'fail', 'bad_name_2.xml'))
-        self.assertFalse(schema.is_valid(os.path.join(script_dir, 'test_cases', 'fail', 'bad_name_2.xml')))
+        test_case_dir = os.path.join(script_dir, 'test_cases', 'fail')
+        test_case_files = [f for f in os.listdir(test_case_dir) if os.path.isfile(os.path.join(test_case_dir, f))]
+
+        for test_file in test_case_files:
+            test_file = os.path.join(test_case_dir, test_file)
+            if schema.is_valid(test_file):
+                # schema validation should fail
+                self.fail('Test case file "{}" passed schema validation when it should have failed!'.format(test_file))
 
     def test_successful_cases(self):
+        """Test cases where the schema validator should succeed"""
         script_dir = os.path.dirname(__file__)
         base_url = os.path.abspath(os.path.join(script_dir, '..', '..', 'leo_mavgen', 'schema'))
         schema = xmlschema.XMLSchema11(os.path.join(base_url, 'mavlink_schema.xsd'), base_url=base_url)
 
-        schema.validate(os.path.join(script_dir, 'test_cases', 'pass', 'all_field_types.xml'))
-        # self.assertTrue(schema.is_valid(os.path.join(script_dir, 'test_cases', 'pass', 'all_field_types.xml')))
+        test_case_dir = os.path.join(script_dir, 'test_cases', 'pass')
+        test_case_files = [f for f in os.listdir(test_case_dir) if os.path.isfile(os.path.join(test_case_dir, f))]
+        # schema.validate(os.path.join(script_dir, 'test_cases', 'pass', 'all_field_types.xml'))
+
+        for test_file in test_case_files:
+            test_file = os.path.join(test_case_dir, test_file)
+            if not schema.is_valid(test_file):
+                # schema validation should pass
+                self.fail('Test case file "{}" failed schema validation when it should have passed!'.format(test_file))
