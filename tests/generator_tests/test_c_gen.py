cce454d664ae637e9d64cd1cf3aa3d392a7347d4
len0rd <len0rd@users.noreply.github.com>
2021-08-05 18:54:28 -0600

    remove useless conda files. fix simple c generator test

--- tests/generator_tests/test_c_gen.py
+++ tests/generator_tests/test_c_gen.py
@@ -8,13 +8,18 @@ class TestCLangGenerator(unittest.TestCase):
     def setUp(self):
         self.validator = MavlinkXmlValidator()
         self.cgen = CLangGenerator()
+        self.base_output_dir = os.path.join(os.path.dirname(__file__), '..', 'test_outputs')
 
     def test(self):
-        simple_xml = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'simple_msg.xml'))
+        simple_xml = os.path.abspath(os.path.join(os.path.dirname(__file__), 'test_cases', 'simple_msg.xml'))
         result = self.validator.validate([simple_xml])
         self.assertIsNotNone(result)
 
-        out_dir = os.path.join(os.path.dirname(__file__), 'c_test_out')
+
+        testName = self.id()
+        testName = testName.split('.')
+        out_dir = os.path.join(self.base_output_dir, *testName)
+        os.makedirs(out_dir)
 
         gen_result = self.cgen.generate(result, out_dir)
         self.assertTrue(gen_result)
